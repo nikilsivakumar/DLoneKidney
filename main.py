bbeb560c53056b017da314e68d5a01ca759a589b
Nikil Sivakumar <nikildevikumar@gmail.com>
2023-12-08 09:43:13 +0530

    Model Training added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 logger.info('Welcome to our custom log')
 
@@ -30,4 +31,15 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
